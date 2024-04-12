d0904e238926d4c9ac111532d31f08b5d415bc8e
someonempty <someonempty@gmail.com>
2024-04-12 12:30:16 +0300

    Adding Sign Up Endpoint.

--- backend/server.js
+++ backend/server.js
@@ -4,26 +4,17 @@ import authRoutes from './routes/auth.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
-dotenv.config();
 const PORT = process.env.PORT || 500;
 
-app.get('/', (req, res) => {
-	res.send('hello world');
-});
+dotenv.config();
+
+app.use(express.json());
 
 app.use("/api/auth/", authRoutes)
 
-app.get('/api/auth/signup', (req, res) => {
-	console.log('signup route');
-});
-
-app.get('/api/auth/login', (req, res) => {
-	console.log('login route');
-});
-
-app.get('/api/auth/logout', (req, res) => {
-	console.log('logout route');
-});
+// app.get('/', (req, res) => {
+// 	res.send('hello world');
+// });
 
 app.listen(PORT, () => {
 	connectToMongoDB()
