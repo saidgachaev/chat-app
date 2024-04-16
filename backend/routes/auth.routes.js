a5ce911ee5e29eec35152f6d4d4ec4b51022628b
someonempty <someonempty@gmail.com>
2024-04-16 11:48:45 +0300

    Adding Get Users For Sidebar Endpoint.

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,5 @@
-import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import express from 'express';
+import { login, logout, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
+
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
@@ -13,15 +16,15 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
-
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
 
 // app.get('/', (req, res) => {
 // 	res.send('hello world');
 // });
 
 app.listen(PORT, () => {
-	connectToMongoDB()
-	console.log(`server works on port ${PORT}`)
+	connectToMongoDB();
+	console.log(`server works on port ${PORT}`);
 });
