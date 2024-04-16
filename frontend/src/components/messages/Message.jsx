c6ec16b6c1c287918f790b60812a3bd280952f4c
someonempty <someonempty@gmail.com>
2024-04-16 13:06:22 +0300

    Adding UI Design.

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,14 @@
+const Message = () => {
+	return (
+		<div>
+			<div className='chat-image avatar'>
+				<div className='w-10 rounded-full'>
+					<img alt='Tailwind CSS chat bubble component' />
+				</div>
+			</div>
+			<div className={`chat-bubble text-white pb-2`}></div>
+			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'></div>
+		</div>
+	);
+};
+export default Message;
