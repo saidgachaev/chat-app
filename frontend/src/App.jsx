c162229f365e291f5ba600ee03ed0660315c70d3
someonempty <someonempty@gmail.com>
2024-04-17 12:40:34 +0300

    Adding Signup Functionality.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
-
+import SignUp from './pages/signup/SignUp';
+import Login from './pages/login/Login';
+import { Toaster } from 'react-hot-toast';
 import './App.css';
 
 function App() {
 	return (
-		<div className='p4 h-screen flex items-center justify-center'>
-			<Home />
+		<div className='p-4 h-screen flex items-center justify-center'>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/signup' element={<SignUp />} />
+				<Route path='/login' element={<Login />} />
+			</Routes>
+			<Toaster />
 		</div>
 	);
 }
